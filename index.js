07869b7bc43c1921311caa90f1f293975cc23f05
Anna Wilson <ajoyna01@gmail.com>
2021-06-10 17:08:54 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "Karen"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "your mom";
+
+}   
+// function changeLeastFavoriteCustomer()
+// //: Now declare a
+// //   constant (using the `const` keyword) in global scope called
+// //   `leastFavoriteCustomer`; be sure to assign it some initial value. Finally,
+// //   write a function called `changeLeastFavoriteCustomer()` that attempts to
+// //   change that constant - notice what JavaScript does when you try to change the
+// //   constant.
